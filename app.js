c52d4ac7185f7a0bd966d95efceadb916bd30017
P-Z-H <83383792+P-Z-H@users.noreply.github.com>
2023-05-21 09:36:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const messages = [
     "It is certain",
     "It is decidedly so",
-    'Without a doubt',
-    'Yes, definitely',
-    'You may rely on it',
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
     'As I see it, yes',
     'Most likely',
     'Outlook good',
